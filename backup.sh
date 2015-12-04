6df9d880151f38e3e9e30188ae2f5143465ffbe3
Aymeric MASSE <aym.masse@gmail.com>
2015-12-04 22:23:33 +0100

    Exercice 2 terminé. Plus de tests necéssaires.

--- backup.sh
+++ backup.sh
@@ -6,19 +6,19 @@ function parseArgs () {
     while [ $# -gt 0 ]
     do
         case $1 in
-            -i) backIgnore=$2; shift 1;;
-            -d) backupDirectory=$2; shift 1;;
+            "-i") backIgnore=$2; shift 1;;
+            "-d") backupDirectory=$2; shift 1;;
         esac
         shift 1
     done
-    
+
     # Debug check of fields values
     if [ $DEBUG -eq 1 ]
     then
         echo "backIgnore = $backIgnore"
         echo "backupDirectory = $backupDirectory"
     fi
-    
+
     # Check if arguments are not missing
     if [ -z $backIgnore ]
     then
@@ -51,11 +51,11 @@ function createIgnoreFilter() {
 function explorer() {
     echo $filter
     local mExplorer=$( find $backupDirectory $filter -type f )
-    
+
     # Set separator to "New Line"
     SAVEIFS=$IFS
     IFS=$(echo -en "\n\b")
-    
+
     # Begin treatment for each file
     for mFile in $mExplorer
     do
@@ -63,47 +63,80 @@ function explorer() {
         then
             echo "Initialize backup for $mFile"
         fi
-        
+
         backupFile $mFile
     done
-    
+
     # Set the IFS with its former value
     IFS=$SAVEIFS
 }
 
-# Archive the file correctly
+# Set all variables in order to archive the file properly
 function backupFile () {
-    if [ ! -L "$1" ] && [ -O "$1" ]
+    mDir=$( dirname $1)
+    mSimpleDirName=$( basename $mDir )
+    mBackupPath="$mDir/.backup"
+    mArchivePath="$mBackupPath/$mArchiveName"
+    mSimpleFileName=$( basename $1 )
+
+    if [ ! -L "$1" ] && [ -O "$1" ] && [ $mSimpleDirName != ".backup" ]
     then
-        mDir=$( dirname $1)
-        mBackupPath="$mDir/.backup"
-        mArchivePath="$mBackupPath/$mArchiveName"
-        mSimpleFileName=$( basename $1 )
-        
         checkBackupDir $mBackupPath
         checkArchive
-        
-        checkType $1
-    
-        tar -uf $mArchivePath -C $mDir $mSimpleFileName
+        checkFileType $1
     fi
 }
 
 # Check type of file and call apropriate function to archive it.
-function checkType () {
-    
+function checkFileType () {
+    mFileType=$( file -i $1)
+
+    if [[ $mFileType == "$1: text"* ]]
+    then
+        backupText $1
+    else
+        backupBinary $1
+    fi
+}
+
+# Backup a binary file
+function backupBinary () {
+    tar -uf $mArchivePath -C $mDir $mSimpleFileName
+}
+
+# Backup a text file
+function backupText () {
+    # Extract the file from the archive
+    if [ -f "$mBackupPath/$mInitialArchiveName" ] && [[ $( tar -tvf "$mBackupPath/$mInitialArchiveName" | grep "$mSimpleFileName" ) == *"$mSimpleFileName"* ]]
+    then
+        tar -f "$mBackupPath/$mInitialArchiveName" -x $mSimpleFileName
+        diff $mSimpleFileName $1 > "$mSimpleFileName.patch"
+        tar -uf $mArchivePath "$mSimpleFileName.patch"
+        #rm "$mSimpleFileName.patch"
+    else
+        tar -uf "$mBackupPath/$mInitialArchiveName" -C $mDir $mSimpleFileName
+
+        if [[ $mInitialArchiveName != $mArchiveName ]]
+        then
+            touch "$mSimpleFileName.patch"
+            tar -uf $mArchivePath "$mSimpleFileName.patch"
+            #rm "$mSimpleFileName.patch"
+        fi
+    fi
+}
+
 
 # Check if the .backup directory exists. If not create it.
 function checkBackupDir () {
     if [ ! -e $1 ]
     then
         mkdir $1
-        
+
         if [ $DEBUG -eq 1 ]
         then
             echo "Created directory $1"
         fi
-        
+
     elif [ ! -d $1 ]
     then
         echo "A file named .backup is preventing this program to create a needed directory with this name. Abort."
@@ -113,11 +146,18 @@ function checkBackupDir () {
 
 # Check if initial archive exists. If yes, create a new archive.
 function checkArchive () {
+    mTime=$( date +%s )
+    mHostname=$( hostname )
+
     if [ -f $mArchivePath ]
     then
-        mTime=$( date %s )
-        mArchiveName="backup_$mTime_$HOST.tar.gz"
-        mArchivePath="$mBackupPath/$mArchiveName"
+        mLastTimeModified=$( date +%s -r $mArchivePath)
+
+        if [ ! $mTime == $mLastTimeModified ]
+        then
+            mArchiveName="backup_$mTime _$mHostname.tar.gz"
+            mArchivePath="$mBackupPath/$mArchiveName"
+        fi
     fi
 }
 
